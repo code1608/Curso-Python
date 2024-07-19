8388f9a6551aedec933fd68e7ee92f2d573c412f
David Ramírez <proyecto2024dev@gmail.com>
2024-07-19 14:58:40 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Curso-Python
 Mi primer curso de manera autodidacta.
+
+Python V 3.0
+
+## Operaciones Básicas
+
+
+## Datos y tipos de datos
+ 
---
+++ hello_world.py
@@ -0,0 +1,29 @@
+#Día 1 - 30 día - python
+
+#Comments of a line
+
+"""
+first pass in python
+
+"""
+
+print("Hello world")
+
+print(2 + 3)     #addition(+)
+print(2 - 3)     #subtraction(-) 
+print(2 * 6)     #multiplication(*)
+print(2 / 3)     #division(/)
+print(10 // 5)   #division int(//)
+print(10 % 5)    #modulus(%)
+print(10 ** 4)   #exponents(**)
+
+#Checking data types
+
+print(type(10))         #Int
+print(type(3.14))       #float
+print(type(1 + 3j))     #complex number
+print(type("Anaballe")) #string
+print(type([1,2,3]))    #list
+print(type({'name':'abase'})) #Dictionary
+print(type({9.0, 2.3, 3.2}))  #set
+print(type((9.0, 'David', 14))) #tuple
